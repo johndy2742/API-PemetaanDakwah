7b9420b7244959c1c3a2b8fcd58d6a87dbc68919
topahadzi <topahadzi@gmail.com>
2023-07-27 21:27:09 +0700

    upload image to do

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -11,14 +11,18 @@ if (!accessKeyId || !secretAccessKey) {
   process.exit(1);
 }
 
-const spaceName = config.do_space_name; // Replace with your actual Space name
-const region = config.do_region; // Replace with your actual region code
-console.log(region);
-const s3 = new AWS.S3({
-  endpoint: `https://sgp1.digitaloceanspaces.com`,
+const spaceName = config.do_space_name;
+const region = config.do_region;
+
+AWS.config.update({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  region: region,
 });
 
-
+const s3 = new AWS.S3({
+  endpoint: `https://${region}.digitaloceanspaces.com`,
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -33,39 +37,32 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single('image');
 
 function uploadImage(req, res) {
-    upload(req, res, function (err) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.status(500).json({ message: 'Error uploading image' });
+    }
+
+    const params = {
+      Bucket: spaceName,
+      Key: req.file.filename,
+      Body: require('fs').createReadStream(req.file.path),
+    };
+
+    console.log('Uploading image with the following params:');
+
+    s3.upload(params, function (err, data) {
       if (err) {
-        return res.status(500).json({ message: 'Error uploading image' });
+        console.error('Error uploading image:', err); // Log the complete error object
+        return res.status(500).json({ message: 'Error uploading image to DigitalOcean Spaces' });
       }
-  
-      AWS.config.update({
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-      });
-  
-      const params = {
-        Bucket: spaceName,
-        Key: req.file.filename,
-        Body: require('fs').createReadStream(req.file.path),
-      };
-  
-      console.log('Uploading image with the following params:');
-      console.log(params);
-  
-      s3.upload(params, function (err, data) {
-        if (err) {
-          console.error('Error uploading image:', err);
-          return res.status(500).json({ message: 'Error uploading image to DigitalOcean Spaces' });
-        }
-  
-        require('fs').unlinkSync(req.file.path);
-  
-        const imageUrl = data.Location;
-        return res.json({ imageUrl: imageUrl });
-      });
+
+      require('fs').unlinkSync(req.file.path);
+
+      const imageUrl = data.Location;
+      return res.json({ imageUrl: imageUrl });
     });
-  }
-  
+  });
+}
 
 const uploadcontroller = {
   uploadImage,
